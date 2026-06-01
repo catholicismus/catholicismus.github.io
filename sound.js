29ce72f14f10e6cb49fe2b37526e673766feeaf5
IOP-Global <arulsheela360@gmail.com>
2026-06-01 12:12:50 +0530

    Update Google Translate element display style

--- sound.js
+++ sound.js
@@ -23,7 +23,7 @@
     container.innerHTML = `
         <div id="subMenu" class="sub-menu" style="display: flex;">
             <button class="sub-btn" onclick="playSound(this)" title="Audio"><svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg></button>
-            <div id="google_translate_element" style="display:none;"></div>
+            <div id="google_translate_element" style="display:block; position:fixed; bottom:100px; right:30px; z-index:99999;"></div>
             <button class="sub-btn" onclick="openTranslate()" title="Translate"><svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04z"/></svg></button>
         </div>
         <button class="main-btn" onclick="toggleMenu()"><svg width="30" height="30" viewBox="0 0 24 24" fill="currentColor"><path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.63l-1.92-3.33c-.12-.22-.38-.31-.61-.22l-2.38.96c-.5-.38-1.03-.7-1.6-.96l-.36-2.52c-.04-.24-.25-.42-.5-.42h-3.84c-.26 0-.47.18-.5-.42l-.36 2.52c-.57.26-1.1.59-1.6.96l-2.38-.96c-.24-.1-.5-.01-.61.22l-1.92 3.33c-.11.22-.06.49.12.63l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.63l1.92 3.33c.12.22.38.31.61.22l2.38-.96c.5.38 1.03.7 1.6.96l.36 2.52c.04.24.25.42.5.42h3.84c.26 0 .47-.18.5-.42l.36-2.52c.57-.26 1.1-.59 1.6-.96l2.38.96c.24.1.5.01.61-.22l1.92-3.33c.11-.22.06-.49-.12-.63l-2.03-1.58zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z"/></svg></button>
@@ -38,7 +38,7 @@
     window.openTranslate = () => {
         const select = document.querySelector('.goog-te-combo');
         if (select) {
-            select.click();
+            select.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
         } else {
             alert("மொழிபெயர்ப்புத் தளம் தயாராகிறது, ஒரு நிமிடம் காத்திருக்கவும்.");
         }
